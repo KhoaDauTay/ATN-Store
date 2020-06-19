2822b88e266d917e4e22b84a51dd8026a59b42f9
khoa03052000 <62291701+khoa03052000@users.noreply.github.com>
2020-06-20 00:18:47 +0700

    Update urls.py

--- store/urls.py
+++ store/urls.py
@@ -3,13 +3,15 @@ from .views import (
     Home,
     Appointment,
     Booking,
-    SearchResultsView
+    SearchResultsView,
+    View,
 )
 app_name = 'store'
 
 
 urlpatterns = [
     path('', Home, name='home'),
+    path('view', View, name='view'),
     path('booking/', Booking, name="booking"),
     path('appointment', Appointment, name="appointment"),
     path('search/', SearchResultsView.as_view(), name='search_results'),
